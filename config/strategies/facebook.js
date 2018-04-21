23074a90a6eee4d95a4c2758a6d9b12173111beb
poramet <toofloksong@hotmail.com>
2018-04-21 07:33:54 +0700

    change localhost 3000 -> 5000

--- config/strategies/facebook.js
+++ config/strategies/facebook.js
@@ -5,9 +5,9 @@ var user = require('../../app/controllers/user.controller.js');
 
 module.exports = function(){
     passport.use(new FacebookStrategy({
-        clientID: config.facebook.clientID,
-        clientSecret: config.facebook.clientSecret,
-        callbackURL : config.facebook.callbackURL,
+        clientID: '277560409449230',
+        clientSecret: '682df626d836799dee1a751b922fccba' ,
+        callbackURL : 'https://mysterious-plains-29409.herokuapp.com/oauth/facebook/callback',
         profileFields: ['id', 'email', 'name'],
         passReqToCallback: true
     }, function(req, accessToken, refreshToken, profile, done){
