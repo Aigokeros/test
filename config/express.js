46a02ac17f116b3bb8ea0c30e11e4e89aaf8d244
poramet <toofloksong@hotmail.com>
2018-04-21 07:01:02 +0700

    comment require index

--- config/express.js
+++ config/express.js
@@ -39,7 +39,7 @@ module.exports = function(){
 		app.use(express.static('./public'));
 		
 		
-		require('../app/routes/index.routes.js')(app);
+	//	require('../app/routes/index.routes.js')(app);
 		require('../app/routes/user.routes.js')(app);
 		return app;
 }
