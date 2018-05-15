0d98c521ddd38c26de30bee559f704b5b956c65d
poramet <toofloksong@hotmail.com>
2018-05-16 04:10:26 +0700

    add database

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -1,4 +1,3 @@
-
-exports.showCart = function(req, res){
-	res.render('cart');
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b09a135b6b2f8f2219fea44d82b28e07e05605cd3d9e1fb48ed8d1927acf6fb
+size 1907
--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -1,5 +1,3 @@
-var product  = require("../controllers/product.controller");
-
-module.exports = function(app){
-    app.get('/cart', product.showCart);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8707a476646ec909fd6fe8ecaeb7152a73c88e185359339a322b321c3320996
+size 271
