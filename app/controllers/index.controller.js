c32b1733eb9226ac883fb5a2c16ee0471847e69b
poramet <toofloksong@hotmail.com>
2018-05-16 19:30:58 +0700

    back to basic

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8e65832e57280e950f3d1fc38c207e2cb907bd45bc4cd47c12543cab456d28f
-size 1417
+oid sha256:e2f4fbbd743753140f2d0c9fa6855c0aceb152073dfbe2c00e5f5eabe4ca010d
+size 869
